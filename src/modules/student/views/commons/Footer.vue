d3f2c1fae89a6a536d4852ceebb4f1e053b03339
snynyder <57409736+MarioTataje@users.noreply.github.com>
2024-11-14 09:27:36 -0500

    feat: actualizar footer

--- src/modules/student/views/commons/Footer.vue
+++ src/modules/student/views/commons/Footer.vue
@@ -1,31 +1,39 @@
 <template>
-    <footer class="footer-container">
-      <p>© 2024 Dommus - Contacto Soporte</p>
-    </footer>
+  <footer class="footer-container">
+    <p>© 2024 Dommus - <a href="https://outlook.office365.com/mail/" target="_blank" class="support-link">Contacto Soporte</a></p>
+  </footer>
 </template>
-  
-  <script>
-  export default {
-    name: 'Footer'
-  };
-  </script>
-  
-  <style scoped>
-  .footer-container {
-    width: 100%;
-    background-color: white; /* Fondo blanco */
-    text-align: center;
-    padding: 20px 0;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    border-top: 1px solid #ccc; /* Agregamos un borde superior ligero */
-  }
-  
-  p {
-    margin: 0;
-    font-size: 18px;
-    font-weight: bold;
-  }
-  </style>
-  
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+<style scoped>
+.footer-container {
+  width: 100%;
+  background-color: white;
+  text-align: center;
+  padding: 20px 0;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  border-top: 1px solid #ccc;
+}
+
+p {
+  margin: 0;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.support-link {
+  color: inherit;
+  text-decoration: none;
+}
+
+.support-link:hover {
+  text-decoration: underline;
+}
+</style>
